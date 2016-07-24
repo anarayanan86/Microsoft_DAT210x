13b2f96effa459329289987d0481bf13231aff99
Arjun Narayanan <anarayanan86@gmail.com>
2016-07-24 08:32:47 -0700

    Create assignment1.py

---
+++ Module-3---Exploring-Data/assignment1.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+
+# Look pretty...
+matplotlib.style.use('ggplot')
+
+#
+# TODO: Load up the Seeds Dataset into a Dataframe
+# It's located at 'Datasets/wheat.data'
+# 
+# .. your code here ..
+df = pd.read_csv('Datasets/wheat.data')
+
+#
+# TODO: Create a slice of your dataframe (call it s1) that only includes the 'area' and 'perimeter' features
+# 
+# .. your code here ..
+s1 = pd.concat([df.area, df.perimeter], axis = 1)
+
+#
+# TODO: Create another slice of your dataframe (call it s2) that only includes the 'groove' and 'asymmetry' features
+# 
+# .. your code here ..
+s2 = pd.concat([df.groove, df.asymmetry], axis = 1)
+
+#
+# TODO: Create a histogram plot using the first slice, and another histogram plot using the second slice.
+# Be sure to set alpha=0.75
+# 
+# .. your code here ..
+s1.plot.hist(alpha = 0.75)
+s2.plot.hist(alpha = 0.75)
+plt.show()
